840984c55696c4e0a66382887e86afbf021d39a6
andrescastrillonponton-dot <andrescastrillonponton@gmail.com>
2026-05-06 20:31:09 -0500

    aa

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = "mysql-dcosof.alwaysdata.net"; // Servidor de la base de datos
 $user = "dcosof_uevosarticulos";       // Usuario de la base de datos
 $password = "clase1234";               // Contraseña del usuario
-$dbname = "dcosof_uevosarticulos";     // Nombre de la base de datos
+$dbname = "dcosof_nuevosarticulos";     // Nombre de la base de datos
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
